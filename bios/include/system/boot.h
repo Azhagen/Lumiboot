d1c56ce02df21f60f445845df7d71646fe7e2eba
Azhagen <degremont.nicolas03@gmail.com>
2024-12-24 14:57:24 +0100

    Fix build

--- bios/include/system/boot.h
+++ bios/include/system/boot.h
@@ -14,26 +14,5 @@
 #define DEV_NETWORK     0x06
 #define DEV_UNKNOWN     0xFF
 
-// typedef void __far (*boot_handler_t)(void);
-
-// struct __packed ipl_bcv_entry
-// {
-//     uint16_t device_type;
-//     uint16_t status_flags;
-//     boot_handler_t boot_handler;
-//     void __far *description;
-//     uint32_t reserved;
-// };
-
-// typedef struct ipl_bcv_entry boot_entry_t;
-// // typedef struct ipl_bcv_entry bcv_entry_t;
-
-// void boot_init(void);
-
-// uint8_t boot_add_entry(uint8_t block_id);
-// void boot_remove_entry(uint8_t block_id);
-
-// uint8_t boot_get_entry(uint8_t index, boot_entry_t __far*__far* entry);
-
-// // void boot_boot_device(uint8_t device);
-// void boot_enable_emulation(uint8_t drive);
+void boot_device(uint8_t id);
+void boot_start(uint8_t id);
